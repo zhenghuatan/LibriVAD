0a2fbadeb77be0caa347a91adcd845e547b0cc82
Zheng-Hua Tan <47390436+zhenghuatan@users.noreply.github.com>
2026-01-08 14:14:17 +0100

    Update KWT3_vad.py

--- Eval-ViT-MFCC/KWT3_vad.py
+++ Eval-ViT-MFCC/KWT3_vad.py
@@ -1,6 +1,9 @@
 # ViT Code adapted from: https://github.com/lucidrains/vit-pytorch
 # Original author: [Author: lucidrains, github:vit-pytorch]
 
+/* * Original code Copyright (c) 2020 Phil Wang
+ * Licensed under the MIT License
+ */
 
 import torch
 import torch.nn.functional as F
